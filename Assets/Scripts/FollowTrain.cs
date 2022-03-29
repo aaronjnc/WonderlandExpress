5013b89203569e68ac7d5efc4166825cc8769dfb
Aaron <aaronkingnc@att.net>
2022-03-29 11:32:08 -0400

    Following trains

---
+++ Assets/Scripts/FollowTrain.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTrain : MonoBehaviour
+{
+    [SerializeField] private TrackPoint nextPoint;
+    [SerializeField] private Rigidbody2D train;
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, nextPoint.transform.position) < .05)
+        {
+            transform.position = nextPoint.transform.position;
+            nextPoint = nextPoint.chosenNext;
+            transform.right = nextPoint.transform.position - transform.position;
+        }
+        float vel = TrainMovement.Instance.velocity;
+        if (vel != 0)
+            transform.position += transform.right * TrainMovement.Instance.velocity * Time.deltaTime;
+    }
+}
--- Assets/Scripts/TrainMovement.cs
+++ Assets/Scripts/TrainMovement.cs
@@ -84,6 +84,7 @@ public class TrainMovement : MonoBehaviour
                     currentVel = transform.right * velocity;
                 }
                 transform.position = Vector3.SmoothDamp(transform.position, nextPoint.transform.position, ref currentVel, 1, maxVelocity);
+                velocity = currentVel.magnitude;
                 if (Vector3.Distance(transform.position, nextPoint.transform.position) < .1)
                 {
                     transform.position = nextPoint.transform.position;
@@ -110,10 +111,15 @@ public class TrainMovement : MonoBehaviour
                 previousChosen.StopAction();
             }
             transform.right = nextPoint.transform.position - transform.position;
+            velocity = Mathf.Clamp(velocity + Time.deltaTime * acceleration, 0, maxVelocity);
+            transform.position = Vector3.MoveTowards(transform.position, nextPoint.transform.position, velocity * Time.deltaTime);
+            Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
             stopped = false;
         }
         else
         {
+            velocity = 0;
+            currentVel = Vector3.zero;
             stopped = true;
         }
     }
