3bb7a2386763c0ed6d03ae70db0330b6b01e42c7
Aaron <aaronkingnc@att.net>
2022-04-09 01:58:15 -0400

    Updated right side of the map

--- Assets/Scripts/FollowTrain.cs
+++ Assets/Scripts/FollowTrain.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FollowTrain : MonoBehaviour
 {
     public TrackPoint nextPoint;
-    [SerializeField] private Rigidbody2D train;
+    [SerializeField] private Transform train;
+    [SerializeField] private float distanceBehind;
     private void FixedUpdate()
     {
-        if (Vector3.Distance(transform.position, nextPoint.transform.position) < .05)
+        if (Vector3.Distance(transform.position, nextPoint.transform.position) < .06)
         {
             transform.position = nextPoint.transform.position;
             nextPoint = nextPoint.chosenNext;
@@ -16,11 +17,19 @@ public class FollowTrain : MonoBehaviour
         }
         float vel = TrainMovement.Instance.velocity;
         if (vel != 0)
-            transform.position += transform.right * TrainMovement.Instance.velocity * Time.deltaTime;
+            transform.position += transform.right * TrainMovement.Instance.velocity * Time.deltaTime;   
     }
 
     public void SetNextPoint(string name)
     {
         nextPoint = GameObject.Find(name).GetComponent<TrackPoint>();
+        if (-train.up == transform.right)
+        {
+            transform.position = train.position - (transform.right * distanceBehind);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = train.position - (transform.right * distanceBehind);
     }
 }
