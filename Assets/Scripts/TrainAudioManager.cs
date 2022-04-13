9f9709a95465f3ce878bd7bb37fcea01a564a3b0
Aaron <aaronkingnc@att.net>
2022-04-13 15:36:45 -0400

    Train audio functionality

--- Assets/Scripts/TrainAudioManager.cs
+++ Assets/Scripts/TrainAudioManager.cs
@@ -1,20 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class TrainAudioManager : MonoBehaviour
 {
-    [Tooltip("The audio source used for train audio")]
-    public AudioSource trainAudioSource;
+    [Tooltip("The audio source used for train audio"), SerializeField]
+    private AudioSource trainAudioSource;
+    [Tooltip("Audio source for train whistle"), SerializeField]
+    private AudioSource trainWhistle;
+    [Tooltip("Speed up audio clip"), SerializeField]
+    private AudioClip trainAccelerate;
+    [Tooltip("Slow down audio clip"), SerializeField]
+    private AudioClip trainDecelerate;
+    [Tooltip("Train max speed audio clip"), SerializeField]
+    private AudioClip trainRegular;
+    PlayerControls controls;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controls = new PlayerControls();
+        controls.ClickEvents.TrainWhistle.performed += Whistle;
+        controls.ClickEvents.TrainWhistle.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Whistle(CallbackContext ctx)
     {
-        
+        trainWhistle.Play();
+    }
+
+    public void SpeedUp()
+    {
+        if (trainAudioSource.clip != trainAccelerate)
+        {
+            trainAudioSource.clip = trainAccelerate;
+            trainAudioSource.loop = false;
+            trainAudioSource.Play();
+        }
+    }
+
+    public void SlowDown()
+    {
+        if (trainAudioSource.clip != trainDecelerate)
+        {
+            trainAudioSource.clip = trainDecelerate;
+            trainAudioSource.loop = false;
+            trainAudioSource.Play();
+        }
+    }
+    public void ConstantSpeed()
+    {
+        if (trainAudioSource.clip != trainRegular)
+        {
+            trainAudioSource.clip = trainRegular;
+            trainAudioSource.loop = true;
+            trainAudioSource.Play();
+        }
     }
 }
